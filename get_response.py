f9fc858b6b33970b31dba9865738e3023b4c91d3
nantanic@stanford.edu <nantanick@Nantanicks-MacBook-Pro.local>
2018-11-09 10:54:00 -0800

    fix typo

--- get_response.py
+++ get_response.py
@@ -336,7 +336,7 @@ def get_response_dict():
     #             "Sound good? "]]
     
     # bot_texts[7][0] = Reply(bot_id=7, response_id=0, texts={modes.GENERAL:tmp_text}, next_id=[(config.DEFAULT_NO, 1), (config.DEFAULT_OTHERS, 2)])
-    bot_texts[7][1] = Reply(bot_id=7, response_id=1, texts={modes.GENERAL:[["We are part of a research study by Stanford University. If you wish to participate, please register at the link below and enter the code from the end of the survey", "https://stanforduniversity.qualtrics.com/jfe/form/SV_cGZtiXUVLkXHjud.", "If you do not wish to participate, please type \'ok\', and your data will not be used in the study."]]}, next_id=2)
+    bot_texts[7][1] = Reply(bot_id=7, response_id=1, texts={modes.GENERAL:[["We are part of a research study by Stanford University. If you wish to participate, please register at the link below and enter the code from the end of the survey.", "https://stanforduniversity.qualtrics.com/jfe/form/SV_cGZtiXUVLkXHjud.", "If you do not wish to participate, please type \'ok\', and your data will not be used in the study."]]}, next_id=2)
     bot_texts[7][2] = Reply(bot_id=7, response_id=2, texts={modes.GENERAL:[["Awesome!", "May we have your name, please?"]]}, next_id=config.CLOSING_INDEX)
     #bot_texts[7][3] = Reply(bot_id=7, response_id=3, texts={modes.GENERAL:[["May I have your name, please?"]]}, next_id=config.CLOSING_INDEX)
 
