c9c8f4d06f09475e9ff7cfc0161e95839642fe00
Marco Mora-Mendoza <marcom3@stanford.edu>
2019-07-31 11:42:01 -0700

    Create gibberish_filter.py

---
+++ gibberish_filter.py
@@ -0,0 +1,44 @@
+from nltk.corpus import words
+
+GIBBERISH_THRESHOLD = .60
+
+def pct_English(txt):
+    txt = txt.split()
+
+    englishCount = 0
+    totalCount = 0
+    for word in txt:
+        totalCount += 1
+        if word in words.words():
+            englishCount += 1
+
+    return float(englishCount/totalCount)
+
+
+def isGibberish(txt):
+    englishPtg = pct_English(txt)
+    txt = txt.split()
+    if len(txt) == 1:
+        if englishPtg < 1:
+            return True
+    else:
+        if englishPtg < GIBBERISH_THRESHOLD:
+            return True
+    return False
+
+
+if __name__== "__main__":
+    while True:
+        text = input("Write text to filter for gibberish. Enter 0 to quit: " + '\n')
+        if text == '0':
+            break
+        elif len(text) == 0:
+            print("No input. Try again." + '\n')
+            continue
+        else:
+            #print("---> " + str(pct_English(text)*100) + '% of English words in input.' + '\n')
+
+            if isGibberish(text):
+                print("--->[" + text + "] classified as GIBBERISH" + '\n')
+            else:
+                print("--->[" + text + "] classified as ENGLISH" + '\n')
